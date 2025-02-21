03645113618462d26792fde6bcc8dc47eb861874
amanglani2503 <ambermanglani8871@gmail.com>
2025-02-21 20:24:16 +0530

    [Amber] Add. Implemented REST API to return "Hello from BridgeLabz"

--- src/main/java/com/example/SpringLearning/controller/HelloWorldController.java
+++ src/main/java/com/example/SpringLearning/controller/HelloWorldController.java
@@ -1,18 +1,16 @@
 package com.example.SpringLearning.controller;
 
-import com.example.SpringLearning.model.HelloWorld;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/SpringLearning/web")
+@RestController
+@RequestMapping("/SpringLearning")
 public class HelloWorldController {
 
     @GetMapping("/hello")
     public String helloPage(Model model) {
-        model.addAttribute("message", new HelloWorld("Hello from BridgeLabz"));
-        return "Hello";
+        return "Hello from BridgeLabz";
     }
 }
