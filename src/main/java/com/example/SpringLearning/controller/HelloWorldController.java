ea6417cdd13581944b4aecdd71b3e93b201d01e2
amanglani2503 <ambermanglani8871@gmail.com>
2025-02-21 20:17:08 +0530

    [Amber] Add. Updated HelloWorldController to serve HTML response using Thymeleaf

--- src/main/java/com/example/SpringLearning/controller/HelloWorldController.java
+++ src/main/java/com/example/SpringLearning/controller/HelloWorldController.java
@@ -1,15 +1,18 @@
 package com.example.SpringLearning.controller;
 
+import com.example.SpringLearning.model.HelloWorld;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/SpringLearning")
+@Controller
+@RequestMapping("/SpringLearning/web")
 public class HelloWorldController {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from BridgeLabz";
+    public String helloPage(Model model) {
+        model.addAttribute("message", new HelloWorld("Hello from BridgeLabz"));
+        return "Hello";
     }
 }
---
+++ src/main/java/com/example/SpringLearning/model/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.example.SpringLearning.model;
+
+public class HelloWorld {
+    private String content;
+
+    public HelloWorld(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
